9d0e2a610f844ca974f39d3c513bb6e6f5f3aaad
l1v1aSouza <livia2212@gmial.com>
2024-10-09 16:03:01 -0300

    Criando umm botão para o usuario conseguir sair da aplicação

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from 'express';
 import { Consultas } from './Consultas';
+import { Auth } from './util/Auth';
 
 const router = express.Router();
 
+router.post('/login', Auth.validacaoUsuario);
+
 router.get('/', (req: Request, res: Response) => { res.send('Hello World!') });
 
-router.get('/rota-protegida', (req: Request, res: Response) => { res.send('Rota protegida, se você está vendo essa mensagem é porque está autenticado no sistema') });
+router.get('/rota-protegida', Auth.verifyToken, (req: Request, res: Response) => { res.send('Rota protegida, se você está vendo essa mensagem é porque está autenticado no sistema') });
 
-router.get('/pessoas', Consultas.todos);
+router.get('/pessoas', Auth.verifyToken, Consultas.todos);
 
 export { router }
